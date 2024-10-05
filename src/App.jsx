7eff8090b4b22f643af7446d7db3e8999acb4aa3
Leandro Andrade <leandrade666@gmail.com>
2024-10-05 13:54:01 -0300

    Primeiro app em React com um componente

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Corpo from './Componentes/Corpo'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Corpo></Corpo>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Componentes/Corpo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Corpo() {
+  return (
+    <div>
+        <h1>Primeiro APP em React </h1>
+      <label>Informe seu Nome</label>
+      <br />
+      <input type="text" />
+    </div>
+  );
+}
+
